ce62cf9cc3bbc0f2d0a13a6185c2196b88d5a277
JosephBisso <j.bisso-bi-ela@tu-braunschweig.de>
2023-04-08 17:10:18 +0200

    added vs solution, deepspeech lib and adapted gitignore

---
+++ DocSpeak/MainPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainPage.xaml.h
+// Deklaration der MainPage-Klasse
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace DocSpeak
+{
+	/// <summary>
+	/// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	};
+}
